265300a3f020255e60f8b1e8a528a0b49c25481b
Mb-03 <baratashvilimisho@gmail.com>
2025-03-15 18:39:52 +0400

    added a triangle

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 import pygame
 from constants import *
+from circleshape import CircleShape
+from player import Player
+
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while 1 == 1:
+        # print(player.triangle())
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT:       
                 return
-        pygame.Surface.fill(screen, ("black"))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    
+            
+
+        
+
 
 if __name__ == "__main__":
     main()
